04383305b6f0a6ad7933d9628fe98383b3d13577
Bipeshkhadka <bipeshkhadka2003@gmail.com>
2024-01-25 09:09:20 +0545

    added the portfolio

---
+++ Script.js
@@ -0,0 +1,7 @@
+let btn=document.getElementById('effect');
+
+
+btn.addEventListener('change',()=>{
+  let x=document.getElementById('outer_contener').firstElementChild;
+  document.x.background-color:red;
+})
